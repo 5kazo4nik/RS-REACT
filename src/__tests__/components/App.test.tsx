3692bad99aedfbd879547bb47897c6992ac94270
5kazo4nik <alhimov.oleg2012@yandex.ru>
2023-10-18 15:04:38 +0300

    feat: add vitest

---
+++ src/__tests__/components/App.test.tsx
@@ -0,0 +1,13 @@
+import { expect, test } from 'vitest';
+
+import { render } from '@testing-library/react';
+import App from '../../app/App';
+
+test('demo', () => {
+  expect(true).toBe(true);
+});
+
+test('Renders the main page', () => {
+  render(<App />);
+  expect(true).toBeTruthy();
+});
