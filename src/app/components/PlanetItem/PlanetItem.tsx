5f936fccc48734c2b0647b2fded1e24009363e60
5kazo4nik <alhimov.oleg2012@yandex.ru>
2023-10-31 18:09:32 +0300

    refactor: destructurize planet object in PlanetItem

--- src/app/components/PlanetItem/PlanetItem.tsx
+++ src/app/components/PlanetItem/PlanetItem.tsx
@@ -9,29 +9,29 @@ interface IPlanetItemProps {
 
 export class PlanetItem extends Component<IPlanetItemProps> {
   render(): ReactNode {
-    const planet = this.props.planet;
+    const { name, diameter, orbital_period, population, climate, terrain, surface_water } = this.props.planet;
 
     return (
       <div className={styles.planet}>
-        <h3 className={styles.planet__heading}>{planet.name}</h3>
+        <h3 className={styles.planet__heading}>{name}</h3>
         <p className={styles.planet__diam}>
-          Diameter of this planet is <strong>{planet.diameter} kilometers.</strong>
+          Diameter of this planet is <strong>{diameter} kilometers.</strong>
         </p>
         <ul className={styles.planet__list}>
           <li>
-            <strong>Orbital period:</strong> {planet.orbital_period} days.
+            <strong>Orbital period:</strong> {orbital_period} days.
           </li>
           <li>
-            <strong>Population:</strong> {planet.population} units.
+            <strong>Population:</strong> {population} units.
           </li>
           <li>
-            <strong>Climate:</strong> {planet.climate}.
+            <strong>Climate:</strong> {climate}.
           </li>
           <li>
-            <strong>Terrain:</strong> {planet.terrain}.
+            <strong>Terrain:</strong> {terrain}.
           </li>
           <li>
-            <strong>Surface water:</strong> {planet.surface_water}%.
+            <strong>Surface water:</strong> {surface_water}%.
           </li>
         </ul>
       </div>
